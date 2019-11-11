7bdc9c0eca067dabc4b2d2759c86ab90dc6f4987
dantim1997 <43065678+dantim1997@users.noreply.github.com>
2019-11-11 19:01:01 +0100

    basic logic
    need the 4 images from the center

---
+++ View/IndexView.php
@@ -0,0 +1,52 @@
+<?php 
+require_once("Autoloader.php");
+class IndexView
+{
+	private $IndexController;
+	private $IndexModel;
+
+	public function __construct($indexController, $indexModel)
+	{
+		$this->IndexController = $indexController;
+		$this->IndexModel = $indexModel;
+	}
+
+	//output to html
+	public function output(){
+		$page = "";
+		$page .= $this->Header();
+		$page .= $this->Body();
+		$page .= $this->Footer();
+		return $page;
+	}
+
+	private function Header(){
+		return $this->IndexController->GetConfig()->GetHeader("Index");
+	}
+
+	private function Body(){
+		//setnav()
+		return "<div id='main'>
+		<div class='lol'></div>
+			<div class='RedBar'></div>
+			<div class='HomepageImages'>
+				<image class='Logo' src='Images/Logo.png'>
+				<image class='HomepageImage' src='Images/HomePageImage.png'>
+			</div>
+			<div class='RedBar'></div>
+
+		</div>";
+	}
+
+	private function Footer(){
+		return "
+		<div class='Footer'>
+			<p id='DesignedBy'>Designed by: Chris Lips, Thijs van Tol, Tim Gras, Stan Roozendaal en Stef Robbe
+			<image class='MediaIcons' src='Images/instagram-icon-black.png'>
+			<image class='MediaIcons' src='Images/facebook-icon.png'>
+			</p>
+		</div>
+		</body></html> ";
+	}
+}
+?>
