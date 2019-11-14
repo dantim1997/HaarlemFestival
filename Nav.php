d7cd641639911ab851a5829f267a54a37a9803fd
dantim1997 <43065678+dantim1997@users.noreply.github.com>
2019-11-14 13:32:26 +0100

    Nav bar layout done

--- Nav.php
+++ Nav.php
@@ -9,11 +9,20 @@ class Nav
 	function SetNavBar(){
 		return"
 		<div id='NavBarBorder'>
-			<div class='NavBar'>
 				<div><img class='NavBarImg' src='Images/HaarlemImage.png'></div>
-				<div class='Active'>Home</div>
-				<div class='NavLinks'>Food</div>
-			</div>
+				<div class='Active'><div class='NavActive'>Home</div></div>
+				<div class='NavLinks'><div class='NavText'>Food</div></div>
+				<div class='NavLinks'><div class='NavText'>Dance</div></div>
+				<div class='NavLinks'><div class='NavText'>Historic</div></div>
+				<div class='NavLinks'><div class='NavText'>Jazz</div></div>
+				<div class='NavLinks'><div class='NavText'>Programme</div></div>
+				<div class='Languages'>
+					<img class='LanguagesImages ActiveLanguage' src='Images/EnglishFlag.png'>
+					<img class='LanguagesImages' src='Images/NederlandFlag.jpg'>
+				</div>
+				<div class='ShoppingCart'>
+					<div class='shopcartItems'>1</div>
+				</div>
 		</div>";
 	}
 }
