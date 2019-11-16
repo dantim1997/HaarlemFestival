aac03440f4ca5aa97cad954dad33796ff105718f
TheChrisKip <43065686+TheChrisKip@users.noreply.github.com>
2019-11-16 14:15:21 +0100

    Navbar functioneel voor historic, jazz
    Navbar functioneel voor historic, jazz

--- Nav.php
+++ Nav.php
@@ -10,11 +10,13 @@ class Nav
 		return"
 		<div id='NavBarBorder'>
 				<div><img class='NavBarImg' src='Images/HaarlemImage.png'></div>
-				<div class='Active'><a href='index.php'><div class='NavActive'>Home</div></a></div>
+				<div class='ActiveWrapper'>
+					<div class='Active'><a href='index.php'><div class='NavActive'>Home</div></a></div>
+				</div>
 				<div class='NavLinks'><a href='Food.php'><div class='NavText'>Food</div></a></div>
 				<div class='NavLinks'><a href='Dance.php'><div class='NavText'>Dance</div></a></div>
-				<div class='NavLinks'><a href='Historic.php'><div class='NavText'>Historic</div></a></div>
-				<div class='NavLinks'><a href='Jazz.php'><div class='NavText'>Jazz</div></a></div>
+				<div class='NavLinks'><a href='historicHome.php'><div class='NavText'>Historic</div></a></div>
+				<div class='NavLinks'><a href='jazz.php'><div class='NavText'>Jazz</div></a></div>
 				<div class='NavLinks'><a href='Programme.php'><div class='NavText'>Programme</div></a></div>
 				<div class='Languages'>
 					<img class='LanguagesImages ActiveLanguage' src='Images/EnglishFlag.png'>
