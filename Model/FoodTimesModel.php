36138fd3b0179642742a60d29584785f775b6c8a
Stef <stef.robbe@gmail.com>
2019-11-20 21:34:41 +0100

    currentUser method removed-stef

--- Model/FoodTimesModel.php
+++ Model/FoodTimesModel.php
@@ -2,18 +2,9 @@
 require_once("Autoloader.php");
 class FoodTimesModel
 {
-	private $CurrentUser;
 	function __construct(){
+		
 	}
 
-	//get currentuser
-	public function GetCurrentUser(){
-		return $this->CurrentUser;
-	}
-
-	//set currentuser
-	public function SetCurrentUser($value){
-		$this->CurrentUser = $value;
-	}
 }
 ?>
