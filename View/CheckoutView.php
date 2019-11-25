45f9209a7838406baa1e6dec9b0636f46857f0c3
TheChrisKip <43065686+TheChrisKip@users.noreply.github.com>
2019-11-25 16:08:40 +0100

    Extra velding in checkouttoegevoegd (#34)
    NOG NIET SCALABLE OP meer dan 4 tickets

--- View/CheckoutView.php
+++ View/CheckoutView.php
@@ -53,37 +53,46 @@ class CheckoutView
 							</form>
 					</div
 					-->
+				<div class='bottomHalfCheckout'>
+					<div class='bottomBar'></div>
 
-				<div class='bottomBar'></div>
+						<div class='checkoutControls'>
+							<div class='input'>
+								<select class='dropDown2'>
+									<option value='-'>-</option>
+									<option value='IDEAL'>IDEAL</option>
+									<option value='VISA'>VISA</option>
+									<option value='Paypal'>Paypal</option>
+								</select>					
 
-					<div class='checkoutControls'>
-						<div class='input'>
-							<select class='dropDown2'>
-								<option value='-'>-</option>
-								<option value='IDEAL'>IDEAL</option>
-								<option value='VISA'>VISA</option>
-								<option value='Paypal'>Paypal</option>
-							</select>					
+								<input class='lbltxt' type='text' placeholder='Firstname' name='??????'>
+								<input class='lbltxt' type='text' placeholder='Lastname' name='??????'>
+								<input class='lbltxt' type='text' placeholder='Email' name='??????'>
+								<input class='lbltxt' type='text' placeholder='Address' name='??????'>
+								<input class='lbltxt' type='text' placeholder='Phone number' name='??????'>
+							</div>
 
-							<input class='emailtxt' type='text' placeholder='email' name='??????'>
+							<div class='labels'>
+								<h3 class='selectlbl selectlblextra'>Select payment method</h3>
+								<h3 class='selectlbl'>Enter Firstname</h3>
+								<h3 class='selectlbl'>Enter Lastname</h3>
+								<h3 class='selectlbl'>Enter Email</h3>
+								<h3 class='selectlbl'>Enter Address</h3>
+								<h3 class='selectlbl'>Enter Phone number</h3>
+								<h3 class='selectlbl selectlblextra2'>Special needs</h3>
+							</div>
+
+							<input class='extraNotestxt' type='text' placeholder='Special needs (allergies, wheelchair access, etc.):' name='??????'>
+							
+							<!-- proceed to payment button -->
+							<form method='post' action=''>	
+								<input class='proceedToCheckoutBTN checkoutBTN' type='submit' value='Proceed to payment' name='proceedToPaymentBTN'>
+							</form>
+
 						</div>
-
-						<div class='labels'>
-							<h3 class='selectlbl'>Select payment method</h3>
-							<h3 class='selectlbl'>Enter email</h3>
-						</div>
-
-						<input class='extraNotestxt' type='text' placeholder='Special needs (allergies, wheelchair access, etc.):' name='??????'>
-						
-						<!-- proceed to payment button -->
-						<form method='post' action=''>	
-							<input class='proceedToCheckoutBTN checkoutBTN' type='submit' value='Proceed to payment' name='proceedToPaymentBTN'>
-						</form>
-
-					</div>
-				
-				<h2 id='totallbl'>Total due:</h2>	<!--Hier de totale prijs als var !-->
-
+					
+					<h2 id='totallbl'>Total due:</h2>	<!--Hier de totale prijs als var !-->
+				</div>
 			</div>
 		</div>";
 	}
