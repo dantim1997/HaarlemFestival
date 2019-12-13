ceb8110ea0844379e71e80a3c3ec81324dd2fa0c
dantim1997 <43065678+dantim1997@users.noreply.github.com>
2019-12-13 16:15:45 +0100

    test

--- Autoloader.php
+++ Autoloader.php
@@ -5,11 +5,11 @@ spl_autoload_register(function ($class_name){
 	$dirs = array_filter(glob('*'), 'is_dir');
 	foreach ($dirs as $dir) {
 		if(file_exists ($dir."/".$class_name.'.php')){
-			include "./".$dir."/".$class_name.'.php';
+			include_once "./".$dir."/".$class_name.'.php';
 		}
 	}
 	if(file_exists ($class_name.'.php')){
-		include $class_name.'.php';
+		include_once $class_name.'.php';
 	}
 });
 ?>
