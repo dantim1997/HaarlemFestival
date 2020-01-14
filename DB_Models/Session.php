b3a86e27abffb55d2844e54bccc033e5880e1d13
Stef <43065653+Stef16Robbe@users.noreply.github.com>
2020-01-14 21:15:13 +0100

    comments and code cleanup Stef (food/checkout) (#134)
    * - tickets aanwezig validatie DONE
    
    * opties aanpassen aan aantal tickets die over zijn (select menu verkleinen) DONE
    
    * device transfer
    
    * same ouwe
    
    * vertaling fixie :D
    
    * FoodMain pagina dynamisch
    
    * fix
    
    * CHECKOUT FIXES NOT DONE
    
    * no results found food
    
    * fix
    
    * Delete test.php
    
    * Delete test.php
    
    * yes
    
    * knop fix
    
    * device transfer
    
    * s
    
    * user feedback getest DONE
    
    * device transfer
    
    * fix
    
    * fix
    
    * yerp
    
    * fixes
    
    * Checkout fixes
    
    * Delete test.php
    
    * comments
    
    * Comments and code cleanup
    
    Co-authored-by: Thijs van Tol <43065692+thijsvtol@users.noreply.github.com>

--- DB_Models/Session.php
+++ DB_Models/Session.php
@@ -8,21 +8,24 @@ class Session
 {
 	function __construct()
 	{
-		if(!isset($_SESSION)){
+		if(!isset($_SESSION)) {
 			session_start();
 		}
 	}
 
-	function AddToCart($eventId, $typeEvent, $amount){
-		if(!isset($_SESSION['Tickets'])){
+	// add 'normal' tickets to cart
+	function AddToCart($eventId, $typeEvent, $amount) {
+		if(!isset($_SESSION['Tickets'])) {
 			$_SESSION['Tickets'] = array();
 		}
 		$allCartItems = EncryptionHelper::Decrypt($_SESSION['Tickets']);
 		$newCartItems = array();
 		$added = false;
+
 		foreach ($allCartItems as $cartItem) {
 			// check if ticket already exists in cart
 			if ($eventId == $cartItem['EventId'] && $typeEvent == $cartItem['TypeEvent']) {
+				// if so, update the amount of the ticket and add it back to array
 				$cartItem['Amount'] = intval($cartItem['Amount']) + intval($amount);
 				$newCartItems[] = $cartItem;
 				$added = true;
@@ -32,26 +35,29 @@ class Session
 			}
 		}
 		if(!$added){
+			// if ticket wasn't added previously because it didn't exist yet, add it now
 			$cartItem = array("EventId"=>$eventId,"TypeEvent"=>$typeEvent,"Amount"=>$amount);
 			$newCartItems[] = $cartItem;
 		}
 
+		// clear and update tickets session
 		$_SESSION['Tickets'] = null;
 		$_SESSION['Tickets'] = EncryptionHelper::Encrypt($newCartItems);
 	}
 
 	function AddToCartFood($eventId, $childAmount, $adultAmount, $startTime, $date, $extraInfo) {
 		$typeEvent = 1;
-		if (!isset($_SESSION['Tickets'])){
+		if (!isset($_SESSION['Tickets'])) {
 			$_SESSION['Tickets'] = array();
 		}
 		$allCartItems = EncryptionHelper::Decrypt($_SESSION['Tickets']);
 		$newCartItems = array();
 		$added = false;
+
 		foreach ($allCartItems as $cartItem) {
 			// check if reservation already exists in cart (eventId, startTime and date define if the cartItem already exists in current session)
 			if ($eventId == $cartItem['EventId'] && $startTime == $cartItem['startTime'] && $date == $cartItem['Date']) {
-				// reservation already exists in session
+				// reservation already exists in session, update the amount
 				$cartItem['ChildAmount'] = intval($cartItem['ChildAmount']) + intval($childAmount);
 				$cartItem['AdultAmount'] = intval($cartItem['AdultAmount']) + intval($adultAmount);
 				$newCartItems[] = $cartItem;
@@ -62,16 +68,18 @@ class Session
 			}
 		}
 		if (!$added) {
+			// if reservation wasn't added previously because it didn't exist yet, add it now
 			$cartItem = array("EventId"=>$eventId, "TypeEvent"=>$typeEvent, "ChildAmount"=>$childAmount, "AdultAmount"=>$adultAmount, "StartTime"=>$startTime, "Date"=>$date, "ExtraInfo"=>$extraInfo);
 			$newCartItems[] = $cartItem;
 		}
 
+		// clear and update tickets session
 		$_SESSION['Tickets'] = null;
 		$_SESSION['Tickets'] = EncryptionHelper::Encrypt($newCartItems);
 	}
 
 	function RemoveFromCart($eventId, $typeEvent){
-		if(!isset($_SESSION['Tickets'])){
+		if(!isset($_SESSION['Tickets'])) {
 			$_SESSION['Tickets'] = array();
 		}
 		$allCartItems = EncryptionHelper::Decrypt($_SESSION['Tickets']);
